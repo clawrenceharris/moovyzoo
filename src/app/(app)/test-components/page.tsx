7bcaad02d55c1a27a5169ddca87429c38de19d36
Caleb Harris <clawrenceharris@gmail.com>
2025-08-16 07:39:57 -0400

    fixes

--- src/app/(app)/test-components/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ComponentShowcase } from "@/components/ui/examples/ComponentShowcase";
-
-export default function TestComponentsPage() {
-  return <ComponentShowcase />;
-}
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,5 @@
 // Base UI Components
 export { Button } from "./button";
 export { Input } from "./input";
+export { Form } from "./form";
 export { Card, CardHeader, CardContent, CardFooter } from "./card";
